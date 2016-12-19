bde1b8638d6557fed981f46a4f546468dbb47103
Dan <dan@fat.sh>
2016-12-19 12:33:06 -0600

    (fix): semantic erros and creating example

---
+++ examples/getAppsList.js
@@ -0,0 +1,13 @@
+const MARATHON_API_URL = process.env.MARATHON_API_URL;
+const MARATHON_AUTH_TOKEN = process.env.MARATHON_API_KEY;
+
+const marathonClient = require('../lib/index')(MARATHON_API_URL, {
+    headers: {
+        'Authorization': 'token=' + MARATHON_AUTH_TOKEN
+    }
+});
+
+marathonClient.apps
+  .getList()
+  .then(apps => console.log(apps))
+  .catch(error => console.error(error));
